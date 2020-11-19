b85f9dbaa340ada2e63ff57c4028e658fc77f40d
Elyton Diniz <elytondiniz@hotmail.com>
2020-11-19 10:51:18 -0400

    atualização do projeto_pergutas
    statefullWidget

--- elytongustavooliveiradiniz/projeto_pergutas/lib/main.dart
+++ elytongustavooliveiradiniz/projeto_pergutas/lib/main.dart
@@ -1,37 +1,53 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-class PerguntaApp extends StatelessWidget {
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    perguntaSelecionada++;
+    print(perguntaSelecionada);
+  }
+
   @override
-  final perguntas = [
-    'Qual é sua cor favorita?'
-        'Qual é seu animal favorito?'
-  ];
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é sua cor favorita?',
+      'Qual é seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Pegurtas'),
+          title: Text('Pegurtas1'),
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas[0]),
+            Text(perguntas[perguntaSelecionada]),
             RaisedButton(
               child: Text('Resposta 1'),
-              onPressed: null,
+              onPressed: responder,
             ),
             RaisedButton(
               child: Text('Resposta 2'),
-              onPressed: null,
+              onPressed: responder,
             ),
             RaisedButton(
               child: Text('Resposta 3'),
-              onPressed: null,
-            )
+              onPressed: responder,
+            ),
           ],
         ),
       ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
